5ca0ca544998cb5ced38c4a073908c1cfbcb1260
Estayparadox <pereniguez.joseph@hotmail.fr>
2018-11-25 12:57:27 +0800

    [+] error handling + constraints

--- BMI Calculator/MainController.swift
+++ BMI Calculator/MainController.swift
@@ -33,9 +33,49 @@ class MainController: UIViewController {
         guard let weightInKG = weightTextField.text else { return }
         guard let heightInCM = heightTextField.text else { return }
         
+        // Check if the textfields are empty or not
+        guard let checker1 = weightTextField.text, !checker1.isEmpty else {
+            showAlert()
+            return
+        }
+        
+        guard let checker2 = heightTextField.text, !checker2.isEmpty else {
+            showAlert()
+            return
+        }
+        
+        // Check if the textfields contain only numbers or not
+        let characterset = CharacterSet(charactersIn: ".0123456789")
+        if heightTextField.text!.rangeOfCharacter(from: characterset.inverted) != nil {
+            showAlert()
+            return
+        }
+        
+        if weightTextField.text!.rangeOfCharacter(from: characterset.inverted) != nil {
+            showAlert()
+            return
+        }
+        
+        // Check if the weight or the height are equal to nil or 0
+        if let n = NumberFormatter().number(from: weightTextField.text!) {
+            let f = CGFloat(truncating: n)
+            if f == 0 {
+                showAlertInput()
+                return
+            }
+        }
+        
+        if let n = NumberFormatter().number(from: heightTextField.text!) {
+            let f = CGFloat(truncating: n)
+            if f == 0 {
+                showAlertInput()
+                return
+            }
+        }
+        
+        // Call the CalculatorBMI Struct
         let cm = CalculatorBMI(userWeight: weightInKG, userHeight: heightInCM)
         let result = cm.calculBMIAction()
-        
         
         // g kodé com 1 sal :)
         if (result <= 18.4) {
@@ -51,10 +91,12 @@ class MainController: UIViewController {
             resultLabel.text = "BMI: \(result)"
             stateLabel.text = "Obese"
         } else {
-            resultLabel.text = "You must fill the fields correctly."
-            stateLabel.text = "Example: weight: 68 height: 178"
+            resultLabel.text = "Fill the fields to know your BMI."
+            stateLabel.text = "Example: weight: 68.4 height: 178.2"
         }
     }
+    
+    // MARK: Actions
     
     @IBAction func cancelButton(_ sender: UIButton) {
         
@@ -69,19 +111,29 @@ class MainController: UIViewController {
     
     // MARK: Functions
     
-    // Deprecated
-    func checker(textField: UITextField) -> Bool {
-        guard (!textField.text!.isEmpty) else {
-            return false
-        }
-        return true
+    func showAlert() {
+        resultLabel.text = "Fill the fields to know your BMI."
+        stateLabel.text = "Example: weight: 68.4 height: 178.2"
+        let alertController = UIAlertController(title: "Wrong values", message: "You must fill the fields with numeric values.", preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alertController.addAction(defaultAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func showAlertInput() {
+        resultLabel.text = "Fill the fields to know your BMI."
+        stateLabel.text = "Example: weight: 68.4 height: 178.2"
+        let alertController = UIAlertController(title: "Wrong values", message: "Your weight or your height can not be equal to 0.", preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alertController.addAction(defaultAction)
+        present(alertController, animated: true, completion: nil)
     }
 
 }
 
+// MARK: Extensions
+
 extension MainController: UITextFieldDelegate {
-    
-    // MARK: Extensions
     
     @IBAction func weightDidChange(_ sender: UITextField) {
         if cancelButtonOutlet.isHidden {
@@ -102,3 +154,13 @@ extension MainController: UITextFieldDelegate {
     
 }
 
+// not used
+extension UITextField {
+    var floatValue : Float {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        
+        let nsNumber = numberFormatter.number(from: text!)
+        return nsNumber == nil ? 0.0 : nsNumber!.floatValue
+    }
+}
